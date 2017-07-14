26d966bfbbe06d90249a36f1526c6d522f54c02d
Tyler W <tyler.walser@yahoo.com>
2017-07-14 09:25:43 -0700

    added some styling to css

--- js/data.js
+++ js/data.js
@@ -2,19 +2,19 @@
 
 var projectData = [
   {
-    name:'project 1',
+    name:'Project 1',
     link:'http://www.genericwebsite.com/',
     description:'<p>This is not really my project one, but I can dream!</p>',
     image: 'project1.jpg'
   },
   {
-    name:'project 2',
+    name:'Project 2',
     link:'http://www.blankwebsite.com/',
     description:'<p>This is not really my project two, but I can dream!</p>',
     image: 'project2.jpg'
   },
   {
-    name:'project 3',
+    name:'Project 3',
     link:'http://www.theworldsworstwebsiteever.com/',
     description:'<p>This is not really my project three, but I can dream!</p>',
     image: 'project3.jpg'
