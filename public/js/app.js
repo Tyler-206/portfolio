9dac351281fe6d1fa3a6b1b57bf9d704200cd0c0
Tyler W <tyler.walser@yahoo.com>
2017-07-25 11:35:40 -0700

    added todos for when I am catching up from being sick

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,12 @@
 'use strict';
 //TODO;  Use of at least one .map() method.
- // TODO Use of at least one .reduce() method.
- // TODO IIFE present on at least one .js file.
+// TODO Use of at least one .reduce() method.
+// TODO IIFE present on at least one .js file.
+//TODO We converted the blog into a SPA by using the page.js library to capture clicks and execute a function to modify the page, rather than reloading content from a server.
+// TODO Give your portfolio the same treatment. Update your main nav so that each clickable item is a link to a different url, that is handled by a function as indicated by page.js.
+// TODO Keep your code organized in a M-V-C structure that isolates data management from presentation layer, from the controller that holds it all together.
+
+
 function Project (portfolioDataObj) {
   this.name = portfolioDataObj.name;
   this.link = portfolioDataObj.link;
