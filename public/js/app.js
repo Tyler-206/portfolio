2fd065a5a9ea8319f6568a5b1f4894ff608ba36e
Tyler W <tyler.walser@yahoo.com>
2017-07-26 11:48:46 -0700

    added todos and added Github Token

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,10 @@
 //TODO We converted the blog into a SPA by using the page.js library to capture clicks and execute a function to modify the page, rather than reloading content from a server.
 // TODO Give your portfolio the same treatment. Update your main nav so that each clickable item is a link to a different url, that is handled by a function as indicated by page.js.
 // TODO Keep your code organized in a M-V-C structure that isolates data management from presentation layer, from the controller that holds it all together.
+//TODO Place all code within the proper layer of MVC abstraction and encapsulation.
+//TODO Use a proxied, authenticated ajax call to GitHub's API to get data from the API.
+// TODO Keep your portfolio responsive as it displays API data.
+// TODO -- DONE DO NOT publish your GitHub Token to any public repo.
 
 
 function Project (portfolioDataObj) {
